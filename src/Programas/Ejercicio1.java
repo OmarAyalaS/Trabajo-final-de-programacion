a4d752472668619702fa1fde8652f18fb6ae93e0
omar <omarayalasolis@gmail.com>
2022-07-15 23:16:19 -0500

    Mi primer programa

--- src/Programas/Ejercicio1.java
+++ src/Programas/Ejercicio1.java
@@ -3,5 +3,7 @@ package Programas;
 public class Ejercicio1 {
     public static void main(String[] args) {
         System.out.println("Fundamentos de programacion");
+        System.out.println("Mi primer programa");
+        System.out.println("Sistemas de control de ventas y facturacion");
     }
 }
